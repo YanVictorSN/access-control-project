b3e36881a85c890a69f84124b85f5ad15c9338d7
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-21 15:05:41 -0300

    refactor: added .ui compatibility

--- src/main.py
+++ src/main.py
@@ -3,39 +3,29 @@ from __future__ import annotations
 import sys
 
 import cv2
+from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QImage
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.ui = uic.loadUi('home.ui', self)
 
-        self.VBL = QVBoxLayout()
-
-        self.FeedLabel = QLabel()
-        self.VBL.addWidget(self.FeedLabel)
-
-        self.CancelBTN = QPushButton('Cancel')
-        self.CancelBTN.clicked.connect(self.CancelFeed)
-        self.VBL.addWidget(self.CancelBTN)
+        self.cancel_QPB.clicked.connect(self.CancelFeed)
 
         self.Worker1 = Worker1()
-
         self.Worker1.start()
         self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
-        self.setLayout(self.VBL)
 
     def ImageUpdateSlot(self, Image):
-        self.FeedLabel.setPixmap(QPixmap.fromImage(Image))
+        self.camera_QL.setPixmap(QPixmap.fromImage(Image))
 
     def CancelFeed(self):
         self.Worker1.stop()
@@ -53,7 +43,10 @@ class Worker1(QThread):
                 Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 FlippedImage = cv2.flip(Image, 1)
                 ConvertToQtFormat = QImage(
-                    FlippedImage.data, FlippedImage.shape[1], FlippedImage.shape[0], QImage.Format_RGB888,
+                    FlippedImage.data,
+                    FlippedImage.shape[1],
+                    FlippedImage.shape[0],
+                    QImage.Format_RGB888
                 )
                 Pic = ConvertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
                 self.ImageUpdate.emit(Pic)
