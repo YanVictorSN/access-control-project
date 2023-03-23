c4a84a70cdae9b3c8b5e12d30d358364be1e6746
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-22 22:53:53 -0300

    feat: add editable students

--- src/course_student_list.py
+++ src/course_student_list.py
@@ -22,6 +22,7 @@ class CourseStudentListWindow(QWidget):
     def button_clicked_event(self):
         self.add_student_qPB.clicked.connect(self.add_student)
         self.remove_student_qPB.clicked.connect(self.remove_student)
+        self.student_qTW.itemSelectionChanged.connect(self.load_student_data)
         self.close_qPB.clicked.connect(self.close)
 
     def add_student(self):
@@ -31,7 +32,12 @@ class CourseStudentListWindow(QWidget):
         if not self.is_valid_input(code, name):
             return
 
-        self.insert_student(code, name)
+        row_position, existing_row = self.find_student_row(code)
+        if existing_row is not None:
+            self.ui.student_qTW.setItem(existing_row, 1, QTableWidgetItem(name))
+        else:
+            self.insert_student(row_position, code, name)
+
         self.clear_input_fields()
         self.ui.message_qLB.setText('Estudante adicionado')
 
@@ -60,8 +66,7 @@ class CourseStudentListWindow(QWidget):
 
         return True
 
-    def insert_student(self, code, name):
-        row_position = self.ui.student_qTW.rowCount()
+    def insert_student(self, row_position, code, name):
         self.ui.student_qTW.insertRow(row_position)
         self.ui.student_qTW.setItem(row_position, 0, QTableWidgetItem(code))
         self.ui.student_qTW.setItem(row_position, 1, QTableWidgetItem(name))
@@ -71,6 +76,26 @@ class CourseStudentListWindow(QWidget):
         self.student_name_qLE.clear()
         self.student_code_qLE.setStyleSheet('')
         self.student_name_qLE.setStyleSheet('')
+
+    def find_student_row(self, code):
+        row_position = self.ui.student_qTW.rowCount()
+        existing_row = next(
+            (
+                row
+                for row in range(row_position)
+                if self.ui.student_qTW.item(row, 0).text() == code
+            ),
+            None,
+        )
+        return row_position, existing_row
+
+    def load_student_data(self):
+        selected_items = self.ui.student_qTW.selectedItems()
+        if not selected_items:
+            return
+        row = selected_items[0].row()
+        self.student_code_qLE.setText(self.ui.student_qTW.item(row, 0).text())
+        self.student_name_qLE.setText(self.ui.student_qTW.item(row, 1).text())
 
 
 if __name__ == '__main__':
