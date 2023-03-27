51ad16acfd1670a7795737730f4cc3f3a7240e5a
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-27 10:36:57 -0300

    refactor: changing resources path

--- src/training_gallery.py
+++ src/training_gallery.py
@@ -12,7 +12,7 @@ from PyQt5.QtWidgets import QWidget
 
 CURRENT_FILE_PATH = os.path.abspath(__file__)
 UI_PATH = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'ui', 'training_gallery.ui')
-DATASET_FOLDER = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'training_dataset')
+DATASET_FOLDER = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'resources', 'training_dataset')
 MAX_COLUMNS = 3
 
 
