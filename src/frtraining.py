c9aa763736edacb5393d6fbd2e3dfe0cf220e0a3
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-27 13:16:26 -0300

    refactor: change to one line

--- src/frtraining.py
+++ src/frtraining.py
@@ -116,14 +116,13 @@ class FaceRecognizer:
             process_this_frame = not process_this_frame
 
             for (top, right, bottom, left), name in zip(face_locations, face_names):
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+                top, right, bottom, left = (i * 4 for i in (top, right, bottom, left))
+
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+
                 font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1, cv2.LINE_AA)
                 mark_attendance(name, added_names)
 
             cv2.imshow('Video', frame)
