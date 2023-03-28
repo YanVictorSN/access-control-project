c6d5f070c538ce5ce9b586b6eccf9db8ea7c66d8
vtfeitosa <vfmidia@hotmail.com>
2023-03-28 14:36:25 -0300

    feat: add name = main

--- src/class_reports.py
+++ src/class_reports.py
@@ -16,6 +16,12 @@ class ClassReportsWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = uic.loadUi(UI_PATH, self)
-        self.init_ui()
-        self.button_clicked_event()
+        # self.init_ui()
+        # self.button_clicked_event()
         self.show()
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    Home = ClassReportsWindow()
+    Home.show()
+    App.exec_()
