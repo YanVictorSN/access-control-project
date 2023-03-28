a5411e7a24fe08b1e24e9a1ba5eaca3e5184dc9d
Yan Victor <yanvictorsn@gmail.com>
2023-03-28 05:30:10 -0300

    feat: add function to show classes

--- src/course_attendance_list.py
+++ src/course_attendance_list.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import sys
 from datetime import datetime
+import json
 
 import cv2
 from PyQt5 import uic
@@ -18,15 +19,7 @@ from PyQt5.QtWidgets import QWidget
 CURRENT_FILE_PATH = os.path.abspath(__file__)
 UI_PATH = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'ui', 'course_attendance_list.ui')
 
-students_test = [
-    {'name': 'Bruno', 'code': '0001'},
-    {'name': 'Sara', 'code': '0002'},
-    {'name': 'Vitória', 'code': '0003'},
-    {'name': 'Vinicius', 'code': '0004'},
-    {'name': 'Yan', 'code': '0005'}
-]
-class_test = 'Python 2023.1'
-
+DATABASE_PATH = "database/student_data.json"
 
 class AttendanceList(QWidget):
     def __init__(self, parent=None):
@@ -36,8 +29,10 @@ class AttendanceList(QWidget):
         self.button_clicked_event()
         self.start_attendance_cam()
         self.set_attendance_time()
-        self.set_class_info()
+        self.get_dataset(DATABASE_PATH)
         self.set_student_info()
+        self.set_class_info()
+        self.button_end_attendance_list()
         self.show()
 
     def init_ui(self):
@@ -58,23 +53,35 @@ class AttendanceList(QWidget):
     def get_image(self, image):
         self.camera_qL.setPixmap(QPixmap.fromImage(image))
 
+    def get_dataset(self, path):
+        with open(path) as f:
+            self.database = json.load(f)
+            return self.database
+
     def set_attendance_time(self):
         current_date = datetime.now()
         current_date_formated = current_date.strftime('%d/%m/%Y')
         self.attendance_date_qL.setText(current_date_formated)
 
     def set_class_info(self):
-        self.course_name_qL.setText(class_test)
+        class_info = self.database['classes'][0]
+        self.course_name_qL.setText(f"{ class_info ['class_name']} {class_info['class_year']}")
 
     def set_student_info(self):
-        self.attendence_qTW.setRowCount(len(students_test))
-
-        for i, student in enumerate(students_test):
-            student_name = QTableWidgetItem(student['name'])
-            student_code = QTableWidgetItem(str(student['code']))
+        data_students = self.database["students"]
+        self.attendence_qTW.setRowCount(len(data_students))
+      
+        for i, student in enumerate(data_students):
+            student_name = QTableWidgetItem(student["student_name"])
+            student_code = QTableWidgetItem(str(student['student_code']))
             self.attendence_qTW.setItem(i, 0, student_code)
             self.attendence_qTW.setItem(i, 1, student_name)
-
+    
+    def button_end_attendance_list(self):
+        self.finish_qPB.clicked.connect(self.end_attendance_list)
+    
+    def end_attendance_list(self):
+        self.get_dataset(DATABASE_PATH)
 
 class AttendanceCam(QThread):
     ImageUpdate = pyqtSignal(QImage)
