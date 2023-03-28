af7167da8a058bd1a443fb7c07f605cd719d498c
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-28 16:30:10 -0300

    refactor: changed start_app to be concise

--- start_app.bat
+++ start_app.bat
@@ -1,59 +1,28 @@
-echo off
-chcp 65001
-cls
+@echo off
 
-set "get_parent_dir=cd /d %~dp0"
-set "venv=.venv\Scripts\activate"
-set "pip-compile=python -m piptools compile --upgrade --resolver=backtracking"
-set "pip-sync=python -m piptools sync"
-
-set "install_piptools=python -m pip install pip-tools"
-set "install_virtualenv=python -m pip install virtualenv"
-set "install_click=python -m pip install --upgrade click"
-
-
-:Menu
-echo Opções:
-echo [1] - Atualizar dependencias
+:menu
+echo Which command sequence do you want to run?
+echo [1] - Atualizar dependencias [No Windows]
 echo [2] - Setup inicial do ambiente [Rodar somente uma vez!]
 echo [3] - Instalar pyenv [No Windows]
-echo  -
-echo [X] - Sair
-echo.
-choice /c 123x /m "Selecione a opção:"
-echo.
+set /p choice=Enter choice number:
 
-goto Option%ERRORLEVEL%
-
-:Option0
-goto Menu
-
-
-:: [1] - Atualizar dependencias
-:Option1
-cls
-echo Atualizando requirements.txt
-cmd /k "%get_parent_dir% & %venv% & %install_click% & %pip-compile% & %pip-sync%"
-
-:: [2] - Setup inicial do ambiente
-:Option2
-cls
-echo Efetuando o setup inicial do ambiente
-%install_piptools%
-%install_virtualenv%
-python -m venv .venv
-cmd /k "%get_parent_dir% & %venv% & %install_piptools% & %install_click% & %pip-compile% & %pip-sync%"
-
-:: [3] - Instalar pyenv
-:Option3
-cls
-echo Instalando pyenv
-set "SCRIPT_PATH=./install-pyenv-win.ps1"
-powershell.exe -Command "& {Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1' -OutFile %SCRIPT_PATH%; &'%SCRIPT_PATH%'}"
-del "install-pyenv-win.ps1"
-goto Menu
-
-
-:: [X] - Sair
-:Option4
-exit /B
+if %choice%==1 (
+  .venv\Scripts\activate
+  pip-compile --upgrade --resolver=backtracking
+  pip-sync
+) else if %choice%==2 (
+  python -m pip install virtualenv
+  virtualenv .venv
+  .venv\Scripts\activate
+  python -m pip install pip-tools
+  pip-compile --upgrade --resolver=backtracking
+  pip-sync
+) else  if %choice%==3 (
+    set "SCRIPT_PATH=./install-pyenv-win.ps1"
+    powershell.exe -Command "& {Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1' -OutFile %SCRIPT_PATH%; &'%SCRIPT_PATH%'}"
+    del "install-pyenv-win.ps1"
+) else (
+  echo Invalid choice, please enter either 1, 2 or 3.
+  goto menu
+)
