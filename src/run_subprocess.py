a73dee975673072630c6cae532f2c2ccc07d66dc
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-28 16:04:26 -0300

    feat: add linux variant and quotes to path

--- src/run_subprocess.py
+++ src/run_subprocess.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 
-VENV_PATH = '.venv'
+VENV_PATH = '.venv' if os.name == 'nt' else 'source .venv'
 VENV_FOLDER = 'Scripts' if os.name == 'nt' else 'bin'
 
 VENV_ACTIVATE = os.path.join(VENV_PATH, VENV_FOLDER, 'activate')
@@ -12,6 +12,6 @@ VENV_PYTHON = os.path.join(VENV_PATH, VENV_FOLDER, 'python')
 
 
 def run_subprocess(*args):
-    arguments = ' '.join(args)
-    command = f'{VENV_ACTIVATE} && {VENV_PYTHON} {arguments}'
+    path_and_arguments = ' '.join(args)
+    command = f'{VENV_ACTIVATE} && {VENV_PYTHON} "{path_and_arguments}"'
     subprocess.Popen(command, shell=True)
