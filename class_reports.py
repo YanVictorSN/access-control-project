3979a9d11fd63e898b5b38376f4ec6c33acfd4da
vtfeitosa <vfmidia@hotmail.com>
2023-03-31 11:46:00 -0300

    feat: add database paths

--- class_reports.py
+++ class_reports.py
@@ -17,6 +17,10 @@ UI_PATH = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'ui', 'ui_class_repor
 ATTENDACE_LIST = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'course_attendance_list.py')
 ATTENDANCE = os.path.join(os.path.dirname(CURRENT_FILE_PATH), 'course_attendance.py')
 
+#Get Json files from DB
+COURSE_DB = os.path.join(CURRENT_FILE_PATH, 'database', 'Course.json')
+# ATTENDANCE_STUDENT = os.path.join(CURRENT_FILE_PATH, 'database', 'Attendance_Student.json')
+
 class ClassReportsWindow(QWidget):
 
     #Starts the Class Reports Window on the screen
@@ -25,17 +29,29 @@ class ClassReportsWindow(QWidget):
         self.ui = uic.loadUi(UI_PATH, self)
         self.init_ui()
         self.button_clicked_event()
+        # self.course_DB = self.get_database(COURSE_DB)
+        # self.course_DB = self.set_classes_info(ATTENDANCE_STUDENT)
+        # self.set_class_info()
+        # self.set_selected_date()
         self.show()   
+
+    #Print Class Info at teh Label
+    # def set_class_info(self):
+    #     self.label.setText('Turma: ' + COURSE_DB['course_name','course_yea'])
 
     #Show Table
     def init_ui(self):
-        self.course_qTW.setHorizontalHeaderLabels(['Código', 'Status', 'Alunos'])
-        self.course_qTW.resizeColumnsToContents()
+        self.reports_qTW.setHorizontalHeaderLabels(['Código', 'Status', 'Alunos'])
+        self.reports_qTW.resizeColumnsToContents()
 
+    # def get_database(self, path):
+    #         with open(path, encoding='utf-8') as f:
+    #             return json.load(f)
+            
     #Assigning events to the buttons
     def button_clicked_event(self):
 
-        #Close class_reports window when called on next lines
+        #Close class_reports window when called
         self.close__qPB.clicked.connect(self.close)
 
         #Throw to the course_attendance window when called on next lines
