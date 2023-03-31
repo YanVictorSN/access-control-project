837c2b96782967b6398eb3bc65084b2986da549b
vtfeitosa <vfmidia@hotmail.com>
2023-03-31 10:59:12 -0300

    fix: changing btn sames

--- class_reports.py
+++ class_reports.py
@@ -35,16 +35,13 @@ class ClassReportsWindow(QWidget):
     #Assigning events to the buttons
     def button_clicked_event(self):
 
-        #Throw to the course_attendance_list window when called on next lines
+        #Close class_reports window when called on next lines
         self.close__qPB.clicked.connect(self.close)
 
         #Throw to the course_attendance window when called on next lines
-        self.btn_training.clicked.connect(self.go_to_attendance)
+        self.new_entry_qPB.clicked.connect(self.go_to_course_attendance)
 
-    def go_to_course_attendance_list(self):
-        run_subprocess(ATTENDACE_LIST)
-
-    def go_to_attendance(self):
+    def go_to_course_attendance(self):
         run_subprocess(ATTENDANCE)
 
 if __name__ == '__main__':
